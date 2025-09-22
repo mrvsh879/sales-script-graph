8fda3e56f35c551cfc86ada5fa211c8e156b6187
mrvsh879 <vladislavshatrov61@gmail.com>
2025-09-22 19:01:36 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const repo = 'sales-script-graph' // имя твоего репозитория
+// имя репозитория на GitHub
+const repo = 'sales-script-graph'
 
 export default defineConfig({
   plugins: [react()],
-  base: `/${repo}/`, // критично для GitHub Pages
+  base: `/${repo}/`,
+  resolve: {
+    dedupe: ['react', 'react-dom'],   // <— важно
+    alias: {
+      // опционально, если используешь алиас @ в tsconfig
+      // '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom'],
+  },
 })
