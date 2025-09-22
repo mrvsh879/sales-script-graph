025c5ac0eed8a0fc9be748a4b14cef0092398cc9
mrvsh879 <vladislavshatrov61@gmail.com>
2025-09-22 13:55:50 +0300

    Update useGraphStore.ts

--- src/store/useGraphStore.ts
+++ src/store/useGraphStore.ts
@@ -1,3 +1,3 @@
-import create from 'zustand';
+import { create } from 'zustand';
 
 export const useGraphStore = create((set) => ({ nodes: [], edges: [], setNodes: (nodes:any)=>set({nodes}), setEdges:(edges:any)=>set({edges}) }));
