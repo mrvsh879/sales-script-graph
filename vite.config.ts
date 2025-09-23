c3143f3604581efd0a5f5467d7fd341c963c1247
mrvsh879 <vladislavshatrov61@gmail.com>
2025-09-23 11:30:22 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// временно для отладки
 export default defineConfig({
   plugins: [react()],
-  base: '/sales-script-graph/',   // имя репозитория
-  build: { outDir: 'dist' }       // куда собирать
+  base: '/sales-script-graph/',
+  build: {
+    outDir: 'dist',
+    minify: false,     // выключаем минификацию
+    sourcemap: true    // включаем sourcemap
+  },
+  resolve: {
+    dedupe: ['react', 'react-dom']  // убираем дубликаты React
+  }
 })
