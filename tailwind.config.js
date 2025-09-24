d3e887506b23e98c62b6ae6d9298725093e0a3a8
mrvsh879 <vladislavshatrov61@gmail.com>
2025-09-24 17:08:47 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,15 @@
-export default { content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], theme: { extend: {}, }, plugins: [], };
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class', // можно убрать, если не нужен тёмный режим по классу
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      // тут можно настраивать цвета/шрифты/тени под твой футуристичный UI
+    },
+  },
+  plugins: [],
+};
